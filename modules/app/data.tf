1695a7b48f38078ec4fae3176beef159149d98b2
Ramtin Kazemi <ramtin.kazemi@gmail.com>
2024-01-08 20:42:04 +0000

    fix(workflow): data dependency

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -18,8 +18,30 @@ data "aws_subnets" "private" {
 
 data "aws_eks_cluster" "this" {
   name = var.cluster_name
+  depends_on = [null_resource.check_eks_cluster_active]
 }
 
 data "aws_eks_cluster_auth" "this" {
   name = var.cluster_name
+  depends_on = [null_resource.check_eks_cluster_active]
+}
+
+resource "null_resource" "check_eks_cluster_active" {
+  triggers = {
+    cluster_name = var.cluster_name
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOF
+TIME_OUT=900
+end=$((SECONDS+TIME_OUT))
+while [ $SECONDS -lt $end ]; do
+  status=$(aws eks describe-cluster --name ${var.cluster_name} --region ${local.aws_region} --query 'cluster.status' --output text 2>/dev/null)
+  [ "$status" = "ACTIVE" ] && exit 0
+  sleep 5
+done
+echo "WARNING: Timeout reached!" && exit 1
+EOF
+    interpreter = ["/bin/bash", "-c"]
+  }
 }
